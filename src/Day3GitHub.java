e53c6fea6583bc94768c14fb5b4636c17c37007f
Cihan Cagatay Sancak <sancakcihan@gmail.com>
2023-05-03 14:11:02 +0300

    C.2

--- src/Day3GitHub.java
+++ src/Day3GitHub.java
@@ -2,5 +2,7 @@ public class Day3GitHub {
     public static void main(String[] args) {
 
         System.out.println("3. Gün - 1. Deneme");
+        System.out.println("3. Gün - 2. Deneme");
+
     }
 }
